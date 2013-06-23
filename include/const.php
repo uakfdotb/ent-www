31e5fc29feebad8d4afd4b1575b8c0d984e41e63
Jack Lu <jacklu@lunaghost.com>
2013-06-23 15:06:36 -0500

    Add LICENSE and README, and license/copyright notice.

--- include/const.php
+++ include/const.php
@@ -1,4 +1,25 @@
 <?php
+/*
+
+	ent-www
+	Copyright [2012-2013] [Jack Lu]
+
+	This file is part of the ent-www source code.
+
+	ent-www is free software: you can redistribute it and/or modify
+	it under the terms of the GNU Affero General Public License as published by
+	the Free Software Foundation, either version 3 of the License, or
+	(at your option) any later version.
+
+	ent-www source code is distributed in the hope that it will be useful,
+	but WITHOUT ANY WARRANTY; without even the implied warranty of
+	MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+	GNU Affero General Public License for more details.
+
+	You should have received a copy of the GNU Affero General Public License
+	along with ent-www source code. If not, see <http://www.gnu.org/licenses/>.
+
+*/
 
 $w3mmdCategories = array("treetag" => "Tree Tag", "civwars" => "Civilization Wars", "battleships" => "Battleships", "legionmega" => "Legion TD Mega", "legionmega2" => "Legion TD Mega HR", "castlefight" => "Castle Fight", "cfone" => "Castle Fight 1v1", "rvs" => "Rabbits vs Sheep", "herolinewars" => "Hero Line Wars", "islanddefense" => "Island Defense", "lihl" => "LIHL");
 
