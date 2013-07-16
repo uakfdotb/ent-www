97694a2f34483980c6021270a454bd17ab2ca94d
Jack Lu <jacklu@lunaghost.com>
2013-07-16 15:18:34 -0500

    Update W3MMD category list constant.

--- include/const.php
+++ include/const.php
@@ -21,6 +21,6 @@
 
 */
 
-$w3mmdCategories = array("treetag" => "Tree Tag", "civwars" => "Civilization Wars", "battleships" => "Battleships", "legionmega" => "Legion TD Mega", "legionmega2" => "Legion TD Mega HR", "castlefight" => "Castle Fight", "cfone" => "Castle Fight 1v1", "rvs" => "Rabbits vs Sheep", "herolinewars" => "Hero Line Wars", "islanddefense" => "Island Defense", "lihl" => "LIHL");
+$w3mmdCategories = array("treetag" => "Tree Tag", "civwars" => "Civilization Wars", "battleships" => "Battleships", "legionmega" => "Legion TD Mega", "legionmega2" => "Legion TD Mega HR", "castlefight" => "Castle Fight", "cfone" => "Castle Fight 1v1", "rvs" => "Rabbits vs Sheep", "herolinewars" => "Hero Line Wars", "islanddefense" => "Island Defense", "lihl" => "LIHL", "nwu" => "NWU");
 
 ?>
