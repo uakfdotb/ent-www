46701127c38602b83f442bd917cad4f8a53bb118
Jack Lu <jacklu@lunaghost.com>
2014-01-16 21:07:57 -0500

    Allow filtering replay by bot ID only.

--- findreplay.php
+++ findreplay.php
@@ -28,42 +28,68 @@
 include("include/common.php");
 include("include/dbconnect.php");
 
-if(isset($_REQUEST['player'])) {
-	$player = $_REQUEST['player'];
-	$name = $player;
-	$realm = FALSE;
-	
-	$tokenPos = strpos($player, '@');
-	
-	if($tokenPos !== FALSE) {
-		$name = substr($player, 0, $tokenPos);
-		$realm = substr($player, $tokenPos + 1);
+if(isset($_REQUEST['player']) || isset($_REQUEST['botid'])) {
+	$name = false;
+	$realm = false;
+	$botid = false;
+
+	if(!empty($_REQUEST['player'])) {
+		$player = $_REQUEST['player'];
+		$name = $player;
+		$realm = FALSE;
+
+		$tokenPos = strpos($player, '@');
+
+		if($tokenPos !== FALSE) {
+			$name = substr($player, 0, $tokenPos);
+			$realm = substr($player, $tokenPos + 1);
+		}
 	}
-	
-	$query = "SELECT gameid, games.gamename FROM gameplayers LEFT JOIN games ON gameid = games.id WHERE name = ?";
-	$parameters = array($name);
-	
-	if(!empty($realm)) {
-		$query .= " AND spoofedrealm = ?";
-		$parameters[] = $realm;
+
+	if(!empty($_REQUEST['botid'])) {
+		$botid = $_REQUEST['botid'];
 	}
-	
-	$query .= " ORDER BY gameplayers.id DESC LIMIT 50";
-	$result = databaseQuery($query, $parameters);
-	
-	echo "<ul>";
-	
-	while($row = $result->fetch()) {
-		$gamename = htmlspecialchars($row[1]);
-		echo "<li><a href=\"findstats.php?id={$row[0]}\">$gamename</a></li>";
+
+	if($botid !== false || $name !== false) {
+		$parameters = array();
+
+		if($name !== false) {
+			$query = "SELECT gameid, games.gamename FROM gameplayers LEFT JOIN games ON gameid = games.id WHERE name = ?";
+			$parameters[] = $name;
+
+			if($realm !== false) {
+				$query .= " AND spoofedrealm = ?";
+				$parameters[] = $realm;
+			}
+
+			if($botid !== false) {
+				$query .= " AND gameplayers.botid = ?";
+				$parameters[] = $botid;
+			}
+		} else {
+			$query = "SELECT id, gamename FROM games WHERE botid = ?";
+			$parameters[] = $botid;
+		}
+
+		$query .= " ORDER BY games.id DESC LIMIT 200";
+		$result = databaseQuery($query, $parameters);
+
+		echo "<ul>";
+
+		while($row = $result->fetch()) {
+			$gamename = htmlspecialchars($row[1]);
+			echo "<li><a href=\"findstats.php?id={$row[0]}\">$gamename</a></li>";
+		}
+
+		echo "</ul>";
 	}
-	
-	echo "</ul>";
 }
 
 ?>
 <form method="get" action="findreplay.php">
-Username[@realm] <input type="text" name="player"> <input type="submit" value="Find games/replays">
+Username[@realm] <input type="text" name="player">
+<br />Bot ID: <input type="text" name="botid">
+<br /><input type="submit" value="Find games/replays">
 </form>
 
 </body>
