eecdf7566364d60a0e35015878e8d81fa82d127a
Jack Lu <jacklu@lunaghost.com>
2014-01-16 21:10:51 -0500

    Update W3MMD categories constant.

--- include/const.php
+++ include/const.php
@@ -21,7 +21,7 @@
 
 */
 
-$w3mmdCategories = array("treetag" => "Tree Tag", "civwars" => "Civilization Wars", "battleships" => "Battleships", "legionmega" => "Legion TD Mega", "legionmega2" => "Legion TD Mega HR", "castlefight" => "Castle Fight", "cfone" => "Castle Fight 1v1", "rvs" => "Rabbits vs Sheep", "herolinewars" => "Hero Line Wars", "islanddefense" => "Island Defense", "lihl" => "LIHL", "nwu" => "NWU");
+$w3mmdCategories = array("treetag" => "Tree Tag", "civwars" => "Civilization Wars", "battleships" => "Battleships", "legionmega" => "Legion TD Mega", "legionmega2" => "Legion TD Mega HR", "legionmegaone" => "Legion TD Mega 1v1", "castlefight" => "Castle Fight", "cfone" => "Castle Fight 1v1", "rvs" => "Rabbits vs Sheep", "herolinewars" => "Hero Line Wars", "islanddefense" => "Island Defense", "lihl" => "LIHL", "nwu" => "NWU");
 
 $dotaCategories = array("dota" => "DotA normal", "lod" => "DotA LoD", "eihl" => "ENT DotA League");
 
