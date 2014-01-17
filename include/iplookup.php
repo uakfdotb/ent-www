9098532a36acbced2c0823ee9c94f06b3f3a65f7
Jack Lu <jacklu@lunaghost.com>
2014-01-16 21:11:51 -0500

    Remove whitelist from ip lookup since it is admin-only now, and add last ban time expired.

--- include/iplookup.php
+++ include/iplookup.php
@@ -21,15 +21,13 @@
 
 */
 
-$whitelist = array('173.57.42.213');
-
 function getPlayer($player) {
 	$playerParts = explode('@', $player);
-	
+
 	if(count($playerParts) >= 2) {
 		$name = strtolower($playerParts[0]);
 		$realm = strtolower($playerParts[1]);
-		
+
 		if($realm == "uswest" || $realm == "west") {
 			$realm = "uswest.battle.net";
 		} else if($realm == "useast" || $realm == "east") {
@@ -39,35 +37,31 @@ function getPlayer($player) {
 		} else if($realm == "asia") {
 			$realm = "asia.battle.net";
 		}
-		
+
 		return array($name, $realm);
 	} else {
 		return array($player, "*");
 	}
 }
 
-function isWhitelist($ip) {
-	return in_array($ip, $GLOBALS['whitelist']);
-}
-
 function iplookup($name, $realm, $hours = 336) {
 	$hours = intval($hours);
-	
+
 	$whereRealm = "";
 	$whereArray = array();
-	
+
 	if($realm != "*") {
 		$whereRealm = "AND spoofedrealm = ?";
 		$whereArray = array($realm);
 	}
-	
+
 	return databaseQuery("SELECT DISTINCT ip FROM gameplayers LEFT JOIN games ON games.id = gameplayers.gameid WHERE name = ? $whereRealm AND games.datetime > DATE_SUB( NOW( ), INTERVAL ? HOUR) AND ip != '0.0.0.0' AND ip != '127.0.0.1'", array_merge(array($name), $whereArray, array($hours)));
 }
 
 function simname($name, $hours = 336) {
 	$name = str_replace("*", "%", $name);
 	$hours = intval($hours);
-	
+
 	return databaseQuery("SELECT DISTINCT name, spoofedrealm FROM gameplayers LEFT JOIN games ON games.id = gameplayers.gameid WHERE name LIKE ? AND games.datetime > DATE_SUB( NOW( ), INTERVAL ? HOUR) LIMIT 150", array($name, $hours));
 }
 
@@ -76,46 +70,46 @@ function namelookup($ip) {
 		$parts = explode(".", $ip);
 		$safe_ip = "";
 		$counter = 0;
-	
+
 		foreach($parts as $part) {
 			if(trim($part) != '') {
 				$safe_ip .= intval($part) . ".";
 				$counter++;
 			}
 		}
-		
+
 		if($counter >= 2) {
 			return databaseQuery("SELECT DISTINCT name, spoofedrealm FROM gameplayers WHERE ip LIKE ? ORDER BY id DESC LIMIT 100", array("$safe_ip%"));
 		}
 	}
-	
+
 	return databaseQuery("SELECT DISTINCT name, spoofedrealm FROM gameplayers WHERE ip = ?", array($ip));
 }
 
 function alias($name, $realm, $depth = 1, &$array, $hours = 720, &$iparray = array()) {
 	if($depth > 3) return;
-	
+
 	//set the parameter player as seen
 	$array[$name . '@' . $realm] = true;
-	
+
 	//decrement depth
 	$depth--;
-	
+
 	//find used IP addresses
 	$used_ips = iplookup($name, $realm, $hours);
-	
+
 	while($row = $used_ips->fetch()) {
 		if(!isset($iparray[$row[0]])) {
 			$iparray[$row[0]] = true;
-			
+
 			$names = namelookup($row[0]);
-			
+
 			while($row2 = $names->fetch()) {
 				$player = $row2[0] . '@' . $row2[1];
-				
+
 				if(!isset($array[$player])) {
 					$array[$player] = true;
-					
+
 					if($depth > 0) {
 						alias($row2[0], $row2[1], $depth, $array, $hours, $iparray);
 					}
@@ -128,7 +122,7 @@ function alias($name, $realm, $depth = 1, &$array, $hours = 720, &$iparray = arr
 function lastTimePlayed($name) {
 	$result = databaseQuery("SELECT MAX(games.datetime) FROM gameplayers LEFT JOIN games ON gameplayers.gameid = games.id WHERE gameplayers.name = ?", array($name));
 	$row = $result->fetch();
-	
+
 	if(is_null($row[0])) return "Never";
 	else return $row[0];
 }
@@ -152,6 +146,17 @@ function isBanned($name, $realm) {
 	$result = databaseQuery("SELECT COUNT(*) FROM bans WHERE name = ? AND server = ? AND context = 'ttr.cloud'", array($name, $realm));
 	$row = $result->fetch();
 	return $row[0] == 0 ? "No" : "Yes";
+}
+
+function lastBanExpired($name, $realm) {
+	$result = databaseQuery("SELECT MAX(expiredate) FROM ban_history WHERE name = ? AND server = ?", array($name, $realm));
+	$row = $result->fetch();
+
+	if(empty($row[0])) {
+		return "N/A";
+	} else {
+		return $row[0];
+	}
 }
 
 ?>
