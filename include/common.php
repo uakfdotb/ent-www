bb26ec4c69999f1da4c28e69f26018331efae9ed
Jack Lu <jacklu@entgaming.net>
2014-07-19 14:41:11 -0400

    include/common: add more stuff.

--- include/common.php
+++ include/common.php
@@ -73,33 +73,60 @@ function uxtDate($time = -1) {
 	if($time == -1) {
 		$time = time();
 	}
-	
+
 	return date("j M Y H:i:s T", $time);
 }
 
 function timezoneAbbr($timezone) {
 	$dateTime = new DateTime();
 	$dateTime->setTimeZone(new DateTimeZone($timezone));
-	return $dateTime->format('T'); 
+	return $dateTime->format('T');
 }
 
 function dayDate($time = -1) {
 	if($time == -1) {
 		$time = time();
 	}
-	
+
 	return date("j M Y", $time);
 }
 
 function uid($length) {
 	$characters = "0123456789abcdefghijklmnopqrstuvwxyz";
-	$string = "";	
+	$string = "";
 
 	for ($p = 0; $p < $length; $p++) {
 		$string .= $characters[mt_rand(0, strlen($characters) - 1)];
 	}
 
 	return $string;
+}
+
+function page_requested() {
+	$this_page = basename($_SERVER['REQUEST_URI']);
+	if (strpos($this_page, "?") !== false) {
+		$this_page_array = explode("?", $this_page);
+		$this_page = $this_page_array[0];
+	}
+	return $this_page;
+}
+
+//creates a link or form target based on current URL
+//strips certain GET variables that are specified
+//returns array(link_string, input for form string, unsanitized_link_string)
+// the first two return values are sanitized
+function create_form_target($ignore_get = array()) {
+	$form_string = "";
+	$link_string = "?";
+	foreach($_GET as $key => $val) {
+		if(!in_array($key, $ignore_get)) {
+			$form_string .= '<input type="hidden" name="' . htmlspecialchars($key) . '" value="' . htmlspecialchars($val) . '" />';
+			$link_string .= urlencode($key) . '=' . urlencode($val) . '&';
+		}
+	}
+
+	$link_string = page_requested() . $link_string;
+	return array('link_string' => htmlspecialchars($link_string), 'form_string' => $form_string, 'unsanitized_link_string' => $link_string);
 }
 
 ?>
